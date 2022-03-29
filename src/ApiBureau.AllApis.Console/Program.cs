295112844f1206390545be186ea7746a9da60234
Vaclav Elias <vaclavelias@gmail.com>
2022-03-29 16:22:32 +0100

    Empty lines removed

--- src/ApiBureau.AllApis.Console/Program.cs
+++ src/ApiBureau.AllApis.Console/Program.cs
@@ -9,6 +9,5 @@ var serviceProvider = services.BuildServiceProvider();
 var dataService = serviceProvider.GetService<DataService>()
     ?? throw new ArgumentNullException($"{nameof(DataService)} cannot be null");
 
-
 await dataService.GetStatusAsync();
 await dataService.GetConfluneceSpacesAsync();
